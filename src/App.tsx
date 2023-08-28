ea55a38e4e35033584e55b513d761be114b53d59
thiago <tmg3.mn@gmail.com>
2023-08-28 13:57:24 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from "styled-components/";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyles } from "./styles/global";
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello world</h1>
+      <GlobalStyles />
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: ${(props) => props.theme["gray-600"]};
+    color: ${(props) => props.theme["gray-100"]};
+    font-family: 'Inter', sans-serif;
+    font-weight: 400;
+    -webkit-font-smoothing: antialiased;
+    font-size: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+  }
+`;
