9f6a6ded1e2fd20a859c602ffe9485ac5ec201b2
thiago <tmg3.mn@gmail.com>
2023-09-02 18:10:12 -0300

    Feat: TS logic

--- src/components/InputContainer/index.tsx
+++ src/components/InputContainer/index.tsx
@@ -1,11 +1,32 @@
+import { ChangeEvent, FormEvent, useState } from "react";
 import { Box, Button, Input, InputContainerStyles } from "./styles";
 import { PlusCircle } from "@phosphor-icons/react";
 
 export const InputContainer = () => {
+  const [tasks, setTasks] = useState<string[]>([]);
+  const [newTask, setNewTask] = useState<string>("");
+
+  const handleChangeNewTask = (e: ChangeEvent<HTMLInputElement>) => {
+    setNewTask(e.target.value);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setTasks([...tasks, newTask]);
+    console.log(newTask);
+
+    console.log(tasks);
+  };
+
   return (
-    <InputContainerStyles>
+    <InputContainerStyles onSubmit={handleSubmit}>
       <Box>
-        <Input placeholder="Adicione uma nova tarefa" type="text" />
+        <Input
+          placeholder="Adicione uma nova tarefa"
+          type="text"
+          onChange={handleChangeNewTask}
+          value={newTask}
+        />
         <Button>
           Criar <PlusCircle size={20} />
         </Button>
