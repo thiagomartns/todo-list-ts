2ae3cd109ced37a3ab77abe3e394b3bb136d6af6
thiago <tmg3.mn@gmail.com>
2023-09-02 19:54:37 -0300

    Remove: styles from EmptyTaskList

--- src/components/TaskContainer/EmptyTaskList/styles.ts
+++
@@ -1,8 +0,0 @@
-import { styled } from "styled-components";
-
-export const Parag = styled.p`
-  color: ${(props) => props.theme["gray-300"]};
-  font-style: normal;
-  font-weight: 700;
-  line-height: 1.4;
-`;
